935de0d222f17b635e3e83cb5e6549d48b83b8f4
tangjz <t251346744@gmail.com>
2018-04-02 02:02:30 +0800

    add training 2017-08-15: 2017 Multi-University Training Contest - Team 7

---
+++ 2017-08-15/L.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+#include <cstring>
+typedef long long LL;
+const int maxn = 17, maxs = 1 << 15 | 1, maxv = 14348907, maxp = 7, pr[maxp] = {2, 3, 5, 7, 11, 13, 17};
+int t, n, m, p, low[maxs], upp[maxs], pos[maxs], tr[maxv], f[maxs];
+int choose(int n, int m) {
+	int ans = 1;
+	static int tot, ex[maxp];
+	for(tot = 0; pr[tot] <= m; ++tot) {
+		int tmp = m;
+		for(ex[tot] = 0; tmp >= pr[tot]; ex[tot] += tmp /= pr[tot]);
+	}
+	for(int i = 0; i < m; ++i) {
+		int tmp = n - i;
+		for(int j = 0; j < tot; ++j)
+			for( ; ex[j] && tmp % pr[j] == 0; --ex[j], tmp /= pr[j]);
+		ans = (LL)ans * tmp % p;
+	}
+	return ans;
+}
+int main() {
+	scanf("%d", &t);
+	while(t--) {
+		scanf("%d%d%d", &n, &m, &p);
+		memset(low, 0, (1 << n) * sizeof(int));
+		memset(upp, 0, (1 << n) * sizeof(int));
+		for(int i = 0; i < n; ++i) {
+			int sz, x;
+			scanf("%d", &sz);
+			while(sz--) {
+				scanf("%d", &x);
+				low[1 << i] |= 1 << (--x);
+			}
+			scanf("%d", &sz);
+			while(sz--) {
+				scanf("%d", &x);
+				upp[1 << i] |= 1 << (--x);
+			}
+		}
+		int ans = 0, all = (1 << n) - 1;
+		for(int i = 0; i <= all; ++i) {
+			int lbt = i & -i;
+			if(lbt < i) {
+				low[i] = low[lbt] | low[i ^ lbt];
+				upp[i] = upp[lbt] | upp[i ^ lbt];
+			}
+			int &tot = pos[i + 1];
+			tot = pos[i];
+			for(int j = i; j; j = (j - 1) & i)
+				if((i & low[j]) == low[j] && ((i ^ j ^ all) & upp[j]) == upp[j])
+					tr[tot++] = i ^ j;
+		}
+		memset(f, 0, (1 << n) * sizeof(int));
+		f[0] = 1;
+		for(int i = 1; i <= n && i <= m; ++i) {
+			for(int S = all, it = pos[S + 1] - 1; S >= 0; --S)
+				for(f[S] = 0; it >= pos[S]; --it)
+					(f[S] += f[tr[it]]) >= p && (f[S] -= p);
+			if(f[all])
+				ans = (ans + (LL)f[all] * choose(m, i)) % p;
+		}
+		printf("%d\n", ans);
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -38,6 +38,8 @@ Training list corresponding to the directory.
 
 2017-07-30 [2013 ACM-ICPC Asia Changchun Regional Contest](https://wiki-three-investigators.icpc-camp.org/2013%20ACM-ICPC%20Asia%20Changchun%20Regional%20Contest) (10/10) @chitanda @braveTester
 
+2017-08-15 [2017 Multi-University Training Contest - Team 7](https://wiki-three-investigators.icpc-camp.org/2017%20Multi-University%20Training%20Contest%20-%20Team%207) (13/13) **(lack of B, C-2, E, G, J, K)**
+
 ### Non-Standard Practice
 
 2017-05-24 [2017 ACM-ICPC World Finals](https://wiki-three-investigators.icpc-camp.org/2017%20ACM-ICPC%20World%20Finals) (12/12) **(lack of A-2)**
